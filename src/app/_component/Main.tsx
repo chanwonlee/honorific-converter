fb878f6c4cb3579c27b307f8ebf779982fdb973a
chanwon <clw8679@naver.com>
2024-05-04 23:34:17 +0900

    feat: 번역 버튼 기능 구현

--- src/app/_component/Main.tsx
+++ src/app/_component/Main.tsx
@@ -1,12 +1,31 @@
+"use client";
 import "@/app/globals.css";
-import TranslateForm from "@/app/_component/TranslateForm";
+import React from "react";
 import TranslateResult from "@/app/_component/TranslateResult";
+import TranslateForm from "@/app/_component/TranslateForm";
 
-export default function Main() {
-  return (
-    <div className="flex h-full justify-between">
-      <TranslateForm />
-      <TranslateResult />
-    </div>
-  );
+export default class Main extends React.Component<
+  {},
+  { translatedText: string }
+> {
+  constructor(props: any) {
+    super(props);
+    this.state = { translatedText: "" };
+  }
+
+  handleTranslate = (originText: string) => {
+    const translatedText = "번역 ->" + ' "' + originText + '"';
+    console.log(translatedText);
+    this.setState({ translatedText });
+  };
+
+  render() {
+    // @ts-ignore
+    return (
+      <div className="flex h-full justify-between">
+        <TranslateForm onSubmit={this.handleTranslate} />
+        <TranslateResult translatedText={this.state.translatedText} />
+      </div>
+    );
+  }
 }
--- src/app/_component/TranslateForm.tsx
+++ src/app/_component/TranslateForm.tsx
@@ -1,14 +1,29 @@
+"use client";
 import "@/app/globals.css";
+import { useState } from "react";
 
-export default function TranslateForm() {
+export default ({ onSubmit }: { onSubmit: (text: string) => void }) => {
+  const [inputText, setInputText] = useState("");
+
+  const handleClick = () => {
+    onSubmit(inputText);
+  };
+
   return (
     <div className="mx-8 flex h-full w-full flex-col">
-      <textarea className="flex-grow resize-none rounded-t-md border-l-2 border-r-2 border-t-2 p-10 text-xl focus:outline-none"></textarea>
+      <textarea
+        className="flex-grow resize-none rounded-t-md border-l-2 border-r-2 border-t-2 p-10 text-xl focus:outline-none"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      ></textarea>
       <div className="mb-8 flex h-16 justify-end rounded-b-md border-b-2 border-l-2 border-r-2">
-        <button className="m-0.5 h-full w-20 rounded-full bg-blue-300 hover:bg-blue-400 focus:outline-none">
+        <button
+          className="m-0.5 h-full w-20 rounded-full bg-blue-300 hover:bg-blue-400 focus:outline-none"
+          onClick={handleClick}
+        >
           변환
         </button>
       </div>
     </div>
   );
-}
+};
