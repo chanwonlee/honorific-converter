c63b41c31e5d610f9f5d6d6ba85b2a4ef8390660
chanwon <clw8679@naver.com>
2024-05-12 15:52:31 +0900

    feat: 글자수 표시 기능 추가

--- src/app/_component/TranslateForm.tsx
+++ src/app/_component/TranslateForm.tsx
@@ -4,9 +4,14 @@ import { useState } from "react";
 
 export default ({ onSubmit }: { onSubmit: (text: string) => void }) => {
   const [inputText, setInputText] = useState("");
+  let [inputCount, setInputCount] = useState(0);
 
   const handleClick = () => {
     onSubmit(inputText);
+  };
+
+  const onInputHandler = (e: any) => {
+    setInputCount(e.target.value.length);
   };
 
   return (
@@ -14,9 +19,17 @@ export default ({ onSubmit }: { onSubmit: (text: string) => void }) => {
       <textarea
         className="flex-grow resize-none rounded-t-md border-l-2 border-r-2 border-t-2 p-10 text-xl focus:outline-none"
         value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
+        maxLength={2000}
+        onChange={(e) => {
+          setInputText(e.target.value);
+          onInputHandler(e);
+        }}
       ></textarea>
-      <div className="mb-8 flex h-16 justify-end rounded-b-md border-b-2 border-l-2 border-r-2">
+      <div className="mb-8 flex h-16 justify-between rounded-b-md border-b-2 border-l-2 border-r-2">
+        <p className="pl-2 pt-8">
+          <span className="text-slate-500">{inputCount}</span>
+          <span className="text-slate-500"> / 2000</span>
+        </p>
         <button
           className="m-0.5 h-full w-20 rounded-full bg-blue-300 hover:bg-blue-400 focus:outline-none"
           onClick={handleClick}
